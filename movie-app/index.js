b4460e7ac1c7cd2bca5e25c796ff39123cba02cd
ross-brown <rbrown_98@yahoo.com>
2023-08-14 20:38:45 -0500

    remove some whitespace

--- movie-app/index.js
+++ movie-app/index.js
@@ -14,7 +14,6 @@ $("form").on("submit", (evt) => {
   const movieTitle = $("#movie-title").val();
   const movieRating = $("#movie-rating").val();
 
-
   $(".ratings").append($("<p>", { text: `${movieTitle} has a ratings of ${movieRating}` })
     .append($("<button>", { text: "Remove rating", class: "remove" })));
 
