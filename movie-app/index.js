e47a8698081b7e4060331ef118d10305f4376ba1
ross-brown <rbrown_98@yahoo.com>
2023-08-14 19:29:36 -0500

    create movie app folder

---
+++ movie-app/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+
+$(".ratings").on("click", ".remove", (evt) => {
+  $(evt.target.parentElement).remove();
+});
+
+
+
+$("form").on("submit", (evt) => {
+  evt.preventDefault();
+
+  const movieTitle = $("#movie-title").val();
+  const movieRating = $("#movie-rating").val();
+
+
+  $(".ratings").append($("<p>", { text: `${movieTitle} has a ratings of ${movieRating}` })
+    .append($("<button>", { text: "Remove rating", class: "remove" })));
+
+});
